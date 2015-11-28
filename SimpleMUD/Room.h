5d06e1d204ae1e867d64c69a91a73d386d01d2ae
Ronan Murphy <ronan_murfy@hotmail.com>
2015-11-28 11:43:10 +0000

    Cleaned out base Room. Added vector2 class.

--- SimpleMUD/Room.h
+++ SimpleMUD/Room.h
@@ -35,30 +35,12 @@ class Room : public Entity
 public:
     Room();
 
-    inline RoomType& Type()         { return m_type; }
-    inline int& Data()              { return m_data; }
     inline string& Description()    { return m_description; }
-    inline entityid& Adjacent( int p_dir ) { return m_rooms[p_dir]; }
 
-    inline enemytemplate& SpawnWhich() { return m_spawnwhich; }
-    inline int& MaxEnemies()        { return m_maxenemies; }
-
-    inline list<item>& Items()      { return m_items; }
-    inline money& Money()           { return m_money; }
-
-    inline list<enemy>& Enemies()   { return m_enemies; }
-    inline list<player>& Players()  { return m_players; }
+	inline list<player>& Players()  { return m_players; }
 
     void AddPlayer( player p_player );
     void RemovePlayer( player p_player );
-
-    item FindItem( const string& p_item );
-    void AddItem( item p_item );
-    void RemoveItem( item p_item );
-
-    enemy FindEnemy( const string& p_enemy );
-    void AddEnemy( enemy p_enemy );
-    void RemoveEnemy( enemy p_enemy );
 
     void LoadTemplate( istream& p_stream );
     void LoadData( istream& p_stream );
@@ -69,27 +51,21 @@ protected:
     // -----------------------------------------
     //  template information
     // -----------------------------------------
-    RoomType m_type;
-    int m_data;         // auxilliary data defined by room type
+	string m_name;
     string m_description;
-    entityid m_rooms[NUMDIRECTIONS];
+	vector2 m_coords;
 
-    enemytemplate m_spawnwhich;
-    int m_maxenemies;
 
 
     // -----------------------------------------
     //  volatile data (save to disk)
     // -----------------------------------------
-    list<item> m_items;
-    money m_money;
 
 
     // -----------------------------------------
     //  volatile data (do not save to disk)
     // -----------------------------------------
     list<player> m_players;
-    list<enemy> m_enemies;
 
 
 };  // end class Room
