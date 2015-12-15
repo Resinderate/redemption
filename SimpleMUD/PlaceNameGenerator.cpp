5d038717a1ed257f56fcf03759c4cada88f65ad6
Ronan Murphy <ronan_murfy@hotmail.com>
2015-12-15 14:48:08 +0000

    Added class for generating random place names.

---
+++ SimpleMUD/PlaceNameGenerator.cpp
@@ -0,0 +1,39 @@
+#include "PlaceNameGenerator.h"
+
+std::vector<std::string> SimpleMUD::PlaceNameGenerator::m_prenames;
+std::vector<std::string> SimpleMUD::PlaceNameGenerator::m_postnames;
+
+
+SimpleMUD::PlaceNameGenerator::PlaceNameGenerator()
+{
+}
+
+void SimpleMUD::PlaceNameGenerator::Load()
+{
+	// Load in the two files to the two vectors of strings.
+	std::ifstream prenames("placenames/prenames.txt");
+	std::ifstream postnames("placenames/postnames.txt");
+
+	std::string input;
+
+	while (prenames.good())           
+	{
+		prenames >> input >> std::ws;
+		m_prenames.push_back(input);
+	}
+
+	while (postnames.good())
+	{
+		postnames >> input >> std::ws;
+		m_postnames.push_back(input);
+	}
+}
+
+std::string SimpleMUD::PlaceNameGenerator::GetRandomPlaceName()
+{
+	// Get a random word from each list and return them together to form a place name.
+	int first = rand() % m_prenames.size();
+	int second = rand() % m_postnames.size();
+
+	return m_prenames[first] + m_postnames[second];
+}
---
+++ SimpleMUD/PlaceNameGenerator.h
@@ -0,0 +1,34 @@
+// Class to generate random place names.
+// Ronan
+
+#ifndef PLACENAMEGEN_H
+#define PLACENAMEGEN_H
+
+#include <string>
+#include <vector>
+#include <stdlib.h>
+
+#include "SimpleMUDLogs.h"
+
+namespace SimpleMUD
+{
+	// --------------------------------------------------------------------
+	//  Class for rooms used for collecting resources.
+	// --------------------------------------------------------------------
+	class PlaceNameGenerator
+	{
+	public:
+		PlaceNameGenerator();
+		static void Load();
+		static std::string GetRandomPlaceName();
+
+	protected:
+		static std::vector<std::string> m_prenames;
+		static std::vector<std::string> m_postnames;
+
+	};  // end class PlaceNameGenerator.
+
+}   // end namespace SimpleMUD
+
+
+#endif
